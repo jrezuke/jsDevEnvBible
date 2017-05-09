019e61ba1b7148cc251a4493a265966868710a03
jrezuke <j.rezuke@verizon.net>
2017-05-09 11:21:32 -0400

    init

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+//var chalk = require('chalk'); without a system loader
+import chalk from 'chalk';  //babel handles the loading
+
+
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
